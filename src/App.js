66681cbf1cbfb2ce1ed1129c9af528ef284b3826
AlthafHussainK <althafakm@gmail.com>
2021-05-21 11:05:33 +0530

    add dateBuilder

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", 
+                  "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+
+    return `${day} ${date} ${month} ${year}`
+  }
   return (
     <div className="app">
      <main>
@@ -14,6 +27,16 @@ function App() {
           className="search-bar"
           placeholder="Search.."
           />
+       </div>
+       <div className="location-box">
+         <div className="location">Barcelona, Spain</div>
+         <div className="date">{dateBuilder(new Date())}</div>
+       </div>
+       <div className="weather-box">
+         <div className="temp">
+           15 deg C
+         </div>
+         <div className="weather">Sunny</div>
        </div>
      </main>
     </div>
